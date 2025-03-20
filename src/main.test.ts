4aacadb41e1a2c60eccd2c5edc5cec61fe7d961d
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2025-03-20 17:16:31 +0000

    🧪 Check if calculated recommendations returns a list

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,5 +1,15 @@
 import { it, expect } from 'vitest';
+import type { Car } from './main.types.ts';
+
+const car: Car = {
+  id: 0,
+};
 
 it('can run tests', () => {
   expect(true).toBe(true);
+});
+
+it('returns a list', () => {
+  const res = calcRecomendations(car);
+  expect(Array.isArray(res)).toBe(true);
 });
