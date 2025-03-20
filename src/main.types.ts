9b506d9c41b4fd7a9b555c8432934904dbf11a17
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2025-03-20 20:42:27 -0300

    🏷 Define Station and Position types

--- src/main.types.ts
+++ src/main.types.ts
@@ -1,3 +1,34 @@
 export interface Car {
   id: number;
+  location: Position;
+}
+
+export interface Position {
+  x: number;
+  y: number;
+}
+
+export interface Station {
+  id: number;
+  location: Position;
+}
+
+export function Station(id: number, x: number, y: number): Car {
+  return {
+    id,
+    location: {
+      x,
+      y,
+    },
+  };
+}
+
+export function Car(id: number, x: number, y: number): Car {
+  return {
+    id,
+    location: {
+      x,
+      y,
+    },
+  };
 }
