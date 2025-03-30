fbb62991ec0c277f8af4b3dbdbb853cb29c52bd8
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2025-03-30 18:57:44 -0300

    🚧 Add filterStationsByDistanceRadius

--- src/location.ts
+++ src/location.ts
@@ -1,4 +1,4 @@
-import type { Position } from './main.types.ts';
+import type { Car, Position, Station } from './main.types.ts';
 
 /**
  * Calcula a distancia entre dois pontos
@@ -7,4 +7,15 @@ export function calculateDistance(point1: Position, point2: Position): number {
   const dx = point2.x - point1.x;
   const dy = point2.y - point1.y;
   return Math.sqrt(dx * dx + dy * dy);
+}
+
+export function filterStationsByDistanceRadius(
+  maxRadius: number,
+  stations: Station[],
+  car: Car,
+) {
+  return stations.filter(station => {
+    const distancia = calculateDistance(station.location, car.location);
+    return distancia <= maxRadius;
+  });
 }
