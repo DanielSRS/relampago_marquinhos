d70c7707b8df2583915a82bbac7d4576f9fdca7e
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2025-04-07 00:40:23 -0300

    ✨ Add tcp client

---
+++ apps/car/src/tcp/tcp.ts
@@ -0,0 +1,74 @@
+import * as net from 'node:net';
+import { Logger } from '../utils/utils.js';
+
+type TCTRes =
+	| {
+			type: 'success';
+			data: object;
+	  }
+	| {
+			type: 'error';
+			message: string;
+			error: unknown;
+	  };
+
+const log = Logger.extend('tcpRequest');
+export const tcpRequest = (data: object, host: string, port: number) => {
+	return new Promise<TCTRes>(resolve => {
+		let done = false;
+		const client = new net.Socket();
+		client.connect(port, host, () => {
+			// log.info(`Connected to server at ${HOST}:${PORT}`);
+			client.write(JSON.stringify(data));
+			// log.info(`Sent: `, data);
+		});
+
+		client.on('data', data => {
+			// log.info(`Received: `, JSON.parse(data.toString()));
+			if (typeof data === 'object' && data !== null) {
+				resolve({
+					type: 'success',
+					data: JSON.parse(data.toString()),
+				});
+			} else {
+				resolve({
+					type: 'error',
+					message: 'Invalid data received',
+					error: new Error('Expected object, received: ' + typeof data),
+				});
+			}
+			done = true;
+			client.end();
+		});
+
+		client.on('error', err => {
+			resolve({
+				type: 'error',
+				message: 'Invalid data received',
+				error: err,
+			});
+			log.error(`Error: `, err);
+		});
+
+		client.on('close', hadError => {
+			if (done) {
+				return;
+			}
+			if (hadError) {
+				resolve({
+					type: 'error',
+					message: 'Connection closed with error',
+					error: new Error('Connection closed with error'),
+				});
+				log.error('Connection closed with error');
+				return;
+			}
+			resolve({
+				type: 'error',
+				message: 'Server closed connection',
+				error: new Error('Server closed connection'),
+			});
+			log.error('Connection closed');
+		});
+	});
+};
