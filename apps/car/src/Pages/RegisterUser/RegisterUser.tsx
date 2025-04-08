c7f032bb27d814be546b1c6cd2b8e3ef929c60bf
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2025-04-08 04:53:25 -0300

    🚚 Move constants to its own file

--- apps/car/src/Pages/RegisterUser/RegisterUser.tsx
+++ apps/car/src/Pages/RegisterUser/RegisterUser.tsx
@@ -5,11 +5,8 @@ import TextInput from 'ink-text-input';
 import SelectInput from 'ink-select-input';
 import { tcpRequest } from '../../tcp/tcp.js';
 import Spinner from 'ink-spinner';
+import { FLEX1, SERVER_HOST, SERVER_PORT } from '../../constants.js';
 import type { Request, User, Response } from '../../../../../src/main.types.js';
-
-const FLEX1 = { flexBasis: 0, flexGrow: 1, flexShrink: 1 } as const;
-const SERVER_HOST = 'localhost'; //server IP
-const SERVER_PORT = 8080; // server port
 
 export function RegisterUser(props: { onUserCreated: (user: User) => void }) {
 	const { onUserCreated } = props;
