cadabe5de575e129534bb50568c3419d777e39ce
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2025-04-09 19:59:16 -0300

    ✨ Add stack navigator

--- apps/shared/src/components/index.ts
+++ apps/shared/src/components/index.ts
@@ -2,3 +2,4 @@ export * from './ScrollView/ScrollView.js';
 export * from './View/View.js';
 export * from './resizableRootContainer/resizableRootContainer.js';
 export * from './screen/screen.js';
+export * from './Navigator/Navigator.js';
