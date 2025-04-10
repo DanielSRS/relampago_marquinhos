698ca83802019aa8a9be3c91c7e12d3c9b196497
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2025-04-10 04:17:46 -0300

    ✨ Add PositionCard component for displaying position coordinates

--- apps/shared/src/components/index.ts
+++ apps/shared/src/components/index.ts
@@ -3,3 +3,4 @@ export * from './View/View.js';
 export * from './resizableRootContainer/resizableRootContainer.js';
 export * from './screen/screen.js';
 export * from './Navigator/Navigator.js';
+export * from './position-card/position-card.js';
